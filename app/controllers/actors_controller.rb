42f11df46636f5a14018717628c114af8d17787f
Maurius Hill <146677371+mauriushill@users.noreply.github.com>
2024-02-13 21:42:22 +0000

    finish with project. forgot to commit during process.

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,4 +1,36 @@
 class ActorsController < ApplicationController
+  def update
+      a_id = params.fetch("the_id")
+      match = Actor.where({ :id => a_id })
+      the_id = match.at(0)
+      the_id.name = params.fetch("the_name")
+      the_id.dob = params.fetch("the_dob")
+      the_id.bio = params.fetch("the_bio")
+      the_id.image = params.fetch("the_image")
+      the_id.save
+
+    redirect_to("/actors/#{the_id.id}")
+  end
+
+  def destroy
+    the_id = params.fetch("an_id")
+    matching_record = Actor.where({ :id => the_id})
+    the_actor = matching_record.at(0)
+    the_actor.destroy
+    redirect_to("/actors")
+  end
+
+  def new
+    a = Actor.new
+    a.name = params.fetch("the_name")
+    a.dob = params.fetch("the_dob")
+    a.bio = params.fetch("the_bio")
+    a.image = params.fetch("the_image")
+    a.save
+
+    redirect_to("/actors")
+  end
+
   def index
     matching_actors = Actor.all
     @list_of_actors = matching_actors.order({ :created_at => :desc })
--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,4 +1,39 @@
 class DirectorsController < ApplicationController
+  def update
+    d_id = params.fetch("the_id")
+    matching_record = Director.where({ :id => d_id })
+    d = matching_record.at(0)
+    d.name = params.fetch("the_name")
+    d.dob = params.fetch("the_dob")
+    d.bio = params.fetch("the_bio")
+    d.image = params.fetch("the_image")
+    d.save
+
+    redirect_to("/directors/#{d.id}")
+  end
+
+
+  def destroy
+    the_id = params.fetch("an_id")
+    matching_record = Director.where({ :id => the_id })
+    the_director = matching_record.at(0)
+    the_director.destroy
+
+    redirect_to("/directors")
+  end
+
+
+  def new
+    n = Director.new
+    n.name = params.fetch("new_name")
+    n.dob = params.fetch("dob_entry")
+    n.bio = params.fetch("bio_entry")
+    n.image = params.fetch("image_upload")
+    n.save
+
+    redirect_to("/directors")
+  end
+
   def index
     matching_directors = Director.all
     @list_of_directors = matching_directors.order({ :created_at => :desc })
